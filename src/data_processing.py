7e080896d32500b1cdd8c97dd6daf2b03546d23c
iziefigueiredo <figueiredo_isadora@hotmail.com>
2025-08-26 21:50:36 -0300

    update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -7,7 +7,6 @@ def load_data(path: str) -> pd.DataFrame:
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """Faz limpeza básica das colunas principais."""
     
-
     # Drop duplicados e nulos
     df = df.drop_duplicates()
     df = df.dropna()
@@ -15,11 +14,32 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     #Drop colunas irrelevantes
     df = df.drop(columns=["Unnamed: 0"])
 
+    #========Conversão de tipos========
+    
+    #Retirar " min" e converter Runtime para numérico
     df["Runtime"] = df["Runtime"].str.extract(r"(\d+)").astype(int)
 
     # Released_Year -> numérico 
     df["Released_Year"] = pd.to_numeric(df["Released_Year"], errors="coerce").astype("Int64")
 
+    # Gross -> numérico
+    df["Gross"] = df["Gross"].str.replace(",", "", regex=False).astype("Int64")
+
+    #======== Padronização de categorias ========
+    
+    # Certificate -> padronizar categorias
+    if "Certificate" in df.columns:
+        map_cert = {
+            "U/A": "UA",
+            "Passed": "Approved",
+            "GP": "PG",
+        }
+        df["Certificate"] = df["Certificate"].replace(map_cert)
+
+        main_certs = {"U", "A", "UA", "R", "PG-13", "PG", "G", "Approved", "GP"}
+        df["Certificate"] = df["Certificate"].where(df["Certificate"].isin(main_certs), "Other")
+    
+    
     return df
 
 if __name__ == "__main__":
