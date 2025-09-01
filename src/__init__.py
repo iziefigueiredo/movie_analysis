4e21876aed44c1a8b5ed4ef2bad1cb87c698bcd6
iziefigueiredo <figueiredo_isadora@hotmail.com>
2025-09-01 04:28:10 -0300

    add profile imdb_oscar

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,17 @@
 # src/__init__.py
 
-from .processing_imdb import load_data as load_imdb, clean_data as clean_imdb  #  
-from .processing_oscar import load_data as load_oscar, clean_data as clean_oscar  #  
+from .processing_imdb import load_data as load_imdb, clean_data as clean_imdb
+from .processing_oscar import load_data as load_oscar, clean_data as clean_oscar
 
-from .merge_data import (                                                     #  
+from .merge_data import (
     transform_genres,
     merge_imdb_data,
-    merge_tmdb_imdb,
     transform_oscar_data,
     merge_oscar_data,
 )
 
-from .profile_imdb import run_profile as profile_imdb                          #  
+from .profile_imdb import run_profile as profile_imdb
+from .profile_imdb_oscar import run_profile as profile_imdb_oscar  
 
-from .eda_imdb import run_all as eda_imdb                                      #  
+from .eda_imdb import run_all as eda_imdb
+
